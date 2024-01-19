ab936aaefafec0cb4d589e8c5ae6608fd7254b8c
tammiekoh1518 <e0797744@u.nus.edu>
2024-01-19 18:26:07 +0800

    improved .yml files

--- transform/models/queries/task_5.sql
+++ transform/models/queries/task_5.sql
@@ -18,11 +18,15 @@ top_vendor_gmv AS (
     FROM {{ ref('top_vendor_by_country') }}
     WHERE rank = 1
 )
+
 SELECT
     ctg.country_name,
     tvg.vendor_name,
     tvg.top_vendor_gmv,
     ctg.country_total_gmv,
-    (tvg.top_vendor_gmv / ctg.country_total_gmv) * 100 AS top_vendor_gmv_percentage
+    CASE
+        WHEN ctg.country_total_gmv > 0 THEN (tvg.top_vendor_gmv / ctg.country_total_gmv) * 100
+        ELSE 0
+    END AS top_vendor_gmv_percentage
 FROM top_vendor_gmv tvg
-JOIN country_total_gmv ctg ON tvg.country_name = ctg.country_name;
+INNER JOIN country_total_gmv ctg ON tvg.country_name = ctg.country_name;
